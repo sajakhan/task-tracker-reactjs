cab97dafd9b05b80fdc00718b5009c7997de4bef
sajakhan <71625732+sajakhan@users.noreply.github.com>
2021-10-23 22:55:42 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,75 @@
+// eslint-disable-next-line 
+import Header from './components/Header'
+// eslint-disable-next-line 
+import Tasks from './components/Tasks'
+// eslint-disable-next-line 
+import Add from './components/Add'
+import { useState } from 'react'
+import React from 'react';
+
+
+function App() {
+// eslint-disable-next-line 
+  const [showaddtask,setshowaddtask]= useState(true)
+
+  const [tasks,setTasks] = useState([
+    {
+
+    id:1,
+    text:'Doctor appointment',
+    day:'feb 5th' ,
+    reminder: true
+   },
+   {
+    id:2,
+    text:'football match',
+    day:'feb 6th' ,
+    reminder: true
+
+   },
+   {
+    id:3,
+    text:'meeting appointment',
+    day:'feb 5th' ,
+    reminder: false
+   }
+
+
+])
+// addtask
+const addtask = (task) => {
+  const id = Math.floor(Math.random() * 10000 )+ 1 
+  const newTask = {id, ...task }
+  setTasks([...tasks, newTask])
+}
+
+//Delete task
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task)=> task.id !== id))
+}
+
+const togglereminder = (id) => {
+  setTasks(tasks.map((task) => task.id === id
+  ? {...task, reminder : !task.reminder }
+  : task))
+
+  }
+
+
+
+  return (
+    <div className="App">
+      <Header title="Tracker" onadd={() => setshowaddtask(!showaddtask)} />
+
+      { showaddtask && 
+      <Add onadd={addtask}/>
+      }
+     { tasks.length>0 ? 
+     <Tasks tasks={tasks} onDelete={deleteTask} ontoggle={togglereminder}/>
+     : "no tasks to show"
+     }
+    </div>
+  );
+}
+
+export default App;
